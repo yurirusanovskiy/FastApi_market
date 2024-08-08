659c11de94982748d36394dc1baf9c18e16449e7
yurirusanovskiy <155526474+yurirusanovskiy@users.noreply.github.com>
2024-08-08 09:34:25 +0300

    Update category.py

--- app/routers/category.py
+++ app/routers/category.py
@@ -87,27 +87,3 @@ async def update_category(db: Annotated[AsyncSession, Depends(get_db)], category
             detail='You must be admin user for this'
         )
 
-
-# from fastapi import APIRouter
-#
-# router = APIRouter(prefix='/category', tags=['category'])
-#
-#
-# @router.get('/all_categories')
-# async def get_all_categories():
-#     pass
-#
-#
-# @router.post('/create')
-# async def create_category():
-#     pass
-#
-#
-# @router.put('/update_category')
-# async def update_category():
-#     pass
-#
-#
-# @router.delete('/delete')
-# async def delete_category():
-#     pass
